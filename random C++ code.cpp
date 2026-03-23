203a0dc22c14d59213864062e43aec0d096ba57c
pokikato <akanec155@gmail.com>
2026-03-23 10:30:57 -0400

    Add files via upload

--- random C++ code.cpp
+++ random C++ code.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <random>//help get a random thing from a list
 #include <chrono>
+#include <fstream>
 using namespace std;
 
 //READ ME. This is my first fully working or not working C++ code thing, i am an ametruere to coding and this code may or may not have some errors. Since this is open source i advise you to change the code if theres any errors. i do plan on expanding on the password genertator thingy i just dont have the skills for it yet
@@ -33,11 +34,17 @@ int main(){
     
     }
 
+
     int randomNumber = (rand()%100)+1; //this generates a random ahh number between 0-100
     int secondrandomNumber = (rand()%100)+1; //this also generates a random number
     string randomWord = items[randomIndex]; //from the list of the random ahh words
     string randomSymbols = symbols[randomIndex]; //you get it atp its a random ass symbol 
+    string password= randomNumber<<randomWord<<randomSymbols<<secondrandomNumber + randomNumber;
     cout<<"Your randomly generated password is: \n"; //this is your fuckass password please dont reuse it for everything because it'ss be very easy for someone to hack into your accounts please use atleast 10 random passwords from this thing and write them down because this is also a random thing but slightly predictable
-    cout<< randomNumber<<randomWord<<randomSymbols<<secondrandomNumber + randomNumber; //for this your password will always be a random number between 0-100, a random word from the list i made, a random symbol, another random number from 0-100 + another random number between 0-100.
+    cout<< password;//for this your password will always be a random number between 0-100, a random word from the list i made, a random symbol, another random number from 0-100 + another random number between 0-100.
     return 0;
+
+    ofstream passes("passes.txt"); //this should make a password file which will store some passwords lol
+    passes<< password;
+    passes.close();
 }
